53af14167e444f28263faec26f6ddcdb31158ba5
ankitrlps <rankit92@gmail.com>
2018-07-09 01:36:44 -0400

    Added : WordCountFequency.java

--- Algorithm_DataStructures/src/algorithms/Test.java
+++ Algorithm_DataStructures/src/algorithms/Test.java
@@ -3,31 +3,25 @@ package algorithms;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
 
-/*	private int remove(int[] nums) {
-		for (int i = 0; i < nums.length; i++) {
-			nums[i] = nums[i + 1];
-		}
-
-		return nums.length;
-	}*/
+	/*
+	 * private int remove(int[] nums) { for (int i = 0; i < nums.length; i++) {
+	 * nums[i] = nums[i + 1]; }
+	 * 
+	 * return nums.length; }
+	 */
 
 	public static void main(String[] args) {
-
-		/*int[] nums = new int[] { 1, 1, 2, 3, 4, 6, 7, 8, 9 };
-		int[] newNums = new int[nums.length - 1];
-
-		for (int i = 0; i < newNums.length; i++) {
-			newNums[i] = nums[i + 1];
-		}
-		System.out.println(Arrays.toString(newNums));*/
-		DecimalFormat bd = new DecimalFormat("###.######");
-		double d = 3213.32321d;
-		String res = bd.format(d);
-		System.out.println(res);
+		Map<String, Integer> map = new HashMap<>();
+		map.put("ankit", 1);
+		map.put("rlps", 2);
+		map.put("sis", 3);
 		
+		System.out.println(map.values());
 	}
 
 }
